888381e7f7b1a2c22a452075d05e74e9315ec1d3
Dinkley <devstrpsn@gmail.com>
2019-01-16 05:23:19 +0000

    HUUUUUUUUUUUUUUUUUUU

--- src/main/java/com/dinkley/jigglytatas/JigglytatasListener.java
+++ src/main/java/com/dinkley/jigglytatas/JigglytatasListener.java
@@ -11,7 +11,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.*;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.inventory.PlayerInventory;
+
 
 import static org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult.*;
 
@@ -27,10 +28,10 @@ public class JigglytatasListener implements Listener
         sendJigglyMessageToServer(player.getDisplayName() + " has joined the server.");
 
         event.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE +
-                "[Jigglytatas Server]" + ChatColor.BLUE +
+                "[Jigglytatas]" + ChatColor.BLUE +
                 " Welcome to the Server!");
         event.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE +
-                "[Jigglytatas Server]" + ChatColor.BLUE +
+                "[Jigglytatas]" + ChatColor.BLUE +
                 " Reminder: Felix is Gay");
     }
 
@@ -64,6 +65,9 @@ public class JigglytatasListener implements Listener
     public void PlayerDeathEvent(PlayerDeathEvent event)
     {
         Player player = event.getEntity().getPlayer();
+        PlayerInventory playerInventory = player.getInventory();
+
+        Bukkit.getLogger().info(playerInventory.toString());
 
         //Round down coordinates to the nearest full number, to prevent
         //Insane decimal values like 103.219237891273897
@@ -72,13 +76,14 @@ public class JigglytatasListener implements Listener
         Long zPos = Math.round(player.getLocation().getZ());
         World world = player.getWorld();
 
+
         Bukkit.getLogger().info(player.getName() + " has died at: " + xPos + " " + yPos + " " + zPos + " in dimension: " + world.getName());
         sendJigglyMessageToPlayer(player, "Death Position: " + xPos + " " + yPos + " " + zPos + " in dimension: " + world.getName(), false);
     }
 
 
     @EventHandler
-    public void mobDeath(EntityDeathEvent event)
+    public void entityDeath(EntityDeathEvent event)
     {
         Entity entity = event.getEntity();
         if(entity.getType().toString() == "ENDER_DRAGON")
@@ -111,13 +116,13 @@ public class JigglytatasListener implements Listener
         if (isNight && bedEnterResult == OK)
         {
             //Inform server of player sleeping, and their percentage
-            Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC + "[Jigglytatas Server] " + ChatColor.RESET + "" + ChatColor.GOLD + player.getDisplayName() + " is sleeping. [" + sleepingPlayers + "/" + totalPlayers + "]");
+            Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC + "[Jigglytatas] " + ChatColor.RESET + "" + ChatColor.GOLD + player.getDisplayName() + " is sleeping. [" + sleepingPlayers + "/" + totalPlayers + "]");
 
             if (sleepingPlayers >= halfPlayers)
             {
 
                 //Inform server that night has been skipped, as threshold has been reached
-                Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC + "[Jigglytatas Server] " + ChatColor.RESET + ""  + ChatColor.GOLD + "At least 50% of players are now sleeping. Skipping night.");
+                Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC + "[Jigglytatas] " + ChatColor.RESET + ""  + ChatColor.GOLD + "At least 50% of players are now sleeping. Skipping night.");
 
                 //Set time to 450 ticks (Minecraft Sunrise)
                 world.setTime(450);
@@ -148,12 +153,12 @@ public class JigglytatasListener implements Listener
         if (isError)
         {
             player.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC  +
-                    "[Jigglytatas Server] " + ChatColor.RESET + "" + ChatColor.RED + message);
+                    "[Jigglytatas] " + ChatColor.RESET + "" + ChatColor.RED + message);
         }
         else
         {
             player.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC  +
-                    "[Jigglytatas Server] " + ChatColor.RESET + "" + ChatColor.GOLD + message);
+                    "[Jigglytatas] " + ChatColor.RESET + "" + ChatColor.GOLD + message);
         }
 
     }
@@ -161,7 +166,7 @@ public class JigglytatasListener implements Listener
     public void sendJigglyMessageToServer(String message)
     {
         Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.ITALIC  +
-                "[Jigglytatas Server] " + ChatColor.RESET + "" + ChatColor.GOLD + message);
+                "[Jigglytatas] " + ChatColor.RESET + "" + ChatColor.GOLD + message);
 
 
     }
